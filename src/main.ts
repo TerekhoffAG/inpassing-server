9e56e5c503b39dad612a8af10d8b0cc588ce00d2
alien <terekhoffag@gmail.com>
2022-05-16 23:52:34 +0300

    Добавляет документировании API приложения.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ApiDocumentation } from './utils/api-documentation';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Инициализация документации приложения.
+  ApiDocumentation.initialize(app);
+
   await app.listen(3000);
 }
 bootstrap();
